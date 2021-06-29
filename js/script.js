1c296790b135c9b8304a46872a20b0d33443b56b
Oleksandr2891 <allavrenko@gmail.com>
2021-06-29 22:06:23 +0300

    Выполнено задание 1

--- js/script.js
+++ js/script.js
@@ -78,3 +78,117 @@
 
 //   return message;
 // }
+
+
+// # 24
+// function getDiscount(totalSpent) {
+//   const BASE_DISCOUNT = 0;
+//   const BRONZE_DISCOUNT = 0.02;
+//   const SILVER_DISCOUNT = 0.05;
+//   const GOLD_DISCOUNT = 0.1;
+//   let discount;
+//   // Change code below this line
+// if(totalSpent >= 50000){
+//   discount = GOLD_DISCOUNT;
+// } else if (50000 > totalSpent && totalSpent >= 20000){
+// 	discount = SILVER_DISCOUNT;
+// } else if (20000 > totalSpent && totalSpent >= 5000){
+// 	discount = BRONZE_DISCOUNT;
+// } else {
+// 	discount = BASE_DISCOUNT;
+// }
+//   // Change code above this line
+//   return discount;
+// }
+
+//#27
+
+// function getSubscriptionPrice(type) {
+//   let price;
+//   // Change code below this line
+
+//  switch (type) { // Change this line
+//     case "starter": // Change this line
+//       price = 0; // Change this line
+//       break;
+
+//     case "professional": // Change this line
+//       price = 20; // Change this line
+//       break;
+
+//     case "organization": // Change this line
+//       price = 50; // Change this line
+//       break;
+//   }
+
+//   // Change code above this line
+//   return price;
+// }
+
+
+//#29
+
+// function getShippingCost(country) {
+//   let message;
+//   // Change code below this line
+// 		switch (country) {
+//           case "Australia":
+//             message = "Shipping to Australia will cost 170 credits";
+//             break;
+//           case "China":
+//         	message = "Shipping to China will cost 100 credits";
+//             break;
+//           case "Chile":
+//         	message = "Shipping to Chile will cost 250 credits";
+//             break;
+//           case "Jamaica":
+//         	message = "Shipping to Jamaica will cost 120 credits";
+//             break;
+            
+//           default: 
+//             message = "Sorry, there is no delivery to your country";
+//         }
+//   // Change code above this line
+//   return message;
+// }
+
+// function getNameLength(name) {
+//   const message = `Name ${name} is ${name.length} characters long`; // Change this line
+
+// console.log(message);
+// }
+
+// getNameLength("Poly");
+
+
+
+// #32
+
+// function getSubstring(string, length) {
+//   const substring = string.slice(0, length); // Change this line
+
+//   return substring;
+// }
+
+//  #33
+// function formatMessage(message, maxLength) {
+//   let result;
+//   // Change code below this line
+// if(message.length > maxLength) {
+// 	result = message.slice(0, maxLength) + "...";
+// } else {
+// 	result = message.slice(0, maxLength);
+// }
+//   /// Change code above this line
+//   return result;
+// }
+
+
+// #36 
+// function checkForSpam(message) {
+//   let result;
+//   // Change code below this line
+// 	result = message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale');
+//   // Change code above this line
+//   return result;
+// }
